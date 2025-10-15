95b825e46f7f50389b18569a0d0526dc6b37659b
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2025-10-16 01:26:35 +1100

    fix: Comment out api imports in code_app

--- apps/code_app/views.py
+++ apps/code_app/views.py
@@ -6,7 +6,7 @@ from django.core.paginator import Paginator
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 from .models import CodeExecutionJob, DataAnalysisJob, Notebook, ResourceUsage
-from apps.api.utils import execute_code_safely, generate_analysis_code
+# from apps.api  # Removed - api app not installed.utils import execute_code_safely, generate_analysis_code
 import json
 import threading
 
